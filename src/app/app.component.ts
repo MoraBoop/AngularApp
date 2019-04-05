0fc2340f3d868de4a365546bcd0c02b98d559cd5
Jason <jasonmoramora@outlook.com>
2019-04-05 12:50:49 -0500

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'topdf';
+
+  codigo = new FormControl('');
+  producto = new FormControl('');
+  precio = new FormControl('');
+  descripcion = new FormControl('');
+
+  detalles: Detalles[] = [
+    { codigo: 1, producto: "Arroz", precio: 2.1, descripcion: "Marca saman" },
+    { codigo: 2, producto: "Gaseosa", precio: 2.5, descripcion: "Marca Koka Kola" },
+    { codigo: 3, producto: "Galleta", precio: 5, descripcion: "Orea y morochas" },
+    { codigo: 4, producto: "Aceite", precio: 4, descripcion: "para freir" },
+  ] 
+
+  seleccionar(objeto){
+    this.codigo.setValue(objeto.codigo);
+    this.producto.setValue(objeto.producto);
+    this.precio.setValue(objeto.precio);
+    this.descripcion.setValue(objeto.descripcion);
+  }
+
+  eliminar(){
+    for(let x = 0; x < this.detalles.length; x++){
+      
+    }
+  }
+
+  guardar(){
+
+  }
+
+  actualizar(){
+
+  }
+
+}
+export class Detalles{
+  codigo: number;
+  producto: string;
+  precio: number;
+  descripcion: string;
 }
